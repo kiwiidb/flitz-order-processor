01c2cfe9b17e981d53ea2a21973bfc81d37bba98
Kwinten De Backer <kwinten.de.backer@be-mobile.com>
2019-08-25 11:44:54 +0200

    print body to test

--- webhookhandler.go
+++ webhookhandler.go
@@ -1,6 +1,7 @@
 package storageapi
 
 import (
+	"io/ioutil"
 	"net/http"
 
 	"github.com/sirupsen/logrus"
@@ -9,6 +10,11 @@ import (
 //WebhookHandler to be called by Opennode
 func WebhookHandler(w http.ResponseWriter, r *http.Request) {
 	logrus.Infof("%v", r)
+	bbytes, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "something wrong decoding", http.StatusBadRequest)
+	}
+	logrus.Info(string(bbytes))
 	w.WriteHeader(http.StatusOK)
 	return
 }
